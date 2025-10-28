62648069b9493399a554544cf4f961889350205d
Vedantdevam <vedantdevamsrivastava@gmail.com>
2025-10-29 03:10:14 +0530

    Fix import path for authMiddleware in urlRoutes.js

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { shortenUrl, redirectUrl } from '../controllers/urlController.js';
-import { protect } from './middleware/authMiddleware.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
